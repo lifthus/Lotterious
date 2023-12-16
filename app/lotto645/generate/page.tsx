e751d7a7bc73e2d326f5c9bd4560659c45c0ff48
lifthus <lifthus531@gmail.com>
2023-12-17 00:09:24 +0900

    on number generator

--- app/lotto645/generate/page.tsx
+++ app/lotto645/generate/page.tsx
@@ -2,30 +2,38 @@ export default async function Page() {
   return (
     <main className="flex flex-col min-h-screen items-center">
       <div>
-        <table>
-          <tbody>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-            </tr>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-            </tr>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-            </tr>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-            </tr>
-            <tr>
-              <td>a</td>
-              <td>b</td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <div className="flex">
+            <input type="checkbox" /> 역대 당첨 번호 제외
+          </div>
+          <div className="flex">
+            <input type="checkbox" />{" "}
+            <input
+              type="number"
+              className="border w-10 text-center"
+              min="2"
+              max="6"
+            />
+            개 이상 연속하는 수 제외
+          </div>
+          <div className="flex">
+            <input type="checkbox" />{" "}
+            <input
+              type="number"
+              className="border w-10 text-center"
+              min="2"
+              max="6"
+            />
+            개 이상 연속하는 배수 제외
+          </div>
+          <div className="flex">
+            <input type="checkbox" /> 범위&nbsp;
+            <input type="number" className="border w-10 text-center" />
+            &nbsp;내에&nbsp;
+            <input type="number" className="border w-10 text-center" />개 이상
+            몰린 수 제외
+          </div>
+        </div>
       </div>
     </main>
   );
