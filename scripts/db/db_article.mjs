fdf7a020ebe971c878e76255b8e44f26afc54a4e
lifthus <lifthus531@gmail.com>
2023-12-18 16:38:44 +0900

    misc

--- scripts/db/db_article.mjs
+++ scripts/db/db_article.mjs
@@ -1,6 +1,7 @@
 import { articles } from "./placeholder-article.mjs";
 
 export default async function seedArticleDb(client) {
+  /* article */
   await client.query(`
 CREATE TABLE IF NOT EXISTS articles (
 id BIGSERIAL PRIMARY KEY,
@@ -18,6 +19,7 @@ author_nickname VARCHAR(40) NOT NULL,
 author_password TEXT NOT NULL
 );
   `);
+  // article_likes
   await client.query(`
 CREATE TABLE IF NOT EXISTS article_likes (
 article BIGINT REFERENCES articles(id) NOT NULL,
@@ -25,19 +27,16 @@ ip_addr VARCHAR(40) NOT NULL,
 PRIMARY KEY (article, ip_addr)
 )
   `)
-  await client.query(`
-CREATE OR REPLACE VIEW article_likes_count AS
-SELECT article, COUNT(*) AS likes_count
-FROM article_likes
-GROUP BY article
-  `)
+  // article_like_counts
 await client.query(`
 CREATE OR REPLACE VIEW article_like_counts AS
-SELECT id as article, COUNT(article_likes.article) AS likes_count
+SELECT id as article, COUNT(article_likes.article) AS like_counts
 FROM articles
 LEFT JOIN article_likes ON articles.id = article_likes.article
 GROUP BY articles.id
 `)
+
+/* comment */
   await client.query(`
 CREATE TABLE IF NOT EXISTS article_comments (
 id BIGSERIAL PRIMARY KEY,
@@ -52,6 +51,7 @@ author_nickname VARCHAR(40) NOT NULL,
 author_password TEXT NOT NULL
 );
   `)
+  // reply
   await client.query(`
 CREATE TABLE IF NOT EXISTS comment_replies (
 id BIGSERIAL PRIMARY KEY,
